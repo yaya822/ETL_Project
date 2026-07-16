49aae81f79f11576423ec81afbebfff28264311d
yaya822 <yahyabachri10@gmail.com>
2026-07-16 15:21:45 +0100

    insert to database commit

---
+++ insert_to_mysql/insert_to_mysql.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# read data from csvs files 
+employee_df = pd.read_csv("employee_data.csv")
+vendor_df = pd.read_csv("vendor_data.csv")
+insurance_df=pd.read_csv("insurance_data.csv")
+
+# check dataframes 
+# employee_df
+# vendor_df
+# insurance_df
+
+#create customers  dataframe 
+customers_df=insurance_df[["CUSTOMER_ID","CUSTOMER_NAME","SSN","AGE","CUSTOMER_EDUCATION_LEVEL","ADDRESS_LINE1","ADDRESS_LINE2","CITY","POSTAL_CODE","MARITAL_STATUS","NO_OF_FAMILY_MEMBERS","EMPLOYMENT_STATUS","HOUSE_TYPE"]]
+
+# delete columns from dataframes 
+insurance_df=insurance_df.drop(columns=["CUSTOMER_NAME","SSN","AGE","CUSTOMER_EDUCATION_LEVEL","ADDRESS_LINE1","ADDRESS_LINE2","CITY","POSTAL_CODE","MARITAL_STATUS","NO_OF_FAMILY_MEMBERS","HOUSE_TYPE","EMPLOYMENT_STATUS"])
+del vendor_df["STATE"]
+del employee_df["STATE"]
+
+
+# check dataframes 
+# employee_df
+# vendor_df
+# insurance_df
+# customers_df
+
+# connect to mysql 
+engine = create_engine("mysql+pymysql://datahub:datahub@localhost/insurance_db")
+
+# insert dataframes as tables to mysql
+with engine.connect() as conn:
+    customers_df.to_sql("customers", con=conn, if_exists="replace", index=False)
+    employee_df.to_sql("employee", con=conn, if_exists="replace", index=False)
+    vendor_df.to_sql("vendor", con=conn, if_exists="replace", index=False)
+    insurance_df.to_sql("insurance", con=conn, if_exists="replace", index=False)
+
+print("Succes")
+
